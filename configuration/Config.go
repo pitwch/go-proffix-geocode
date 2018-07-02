fcd7618de51b21378560e7564abf4584d69cbc8a
pitw <5676857+pitw@users.noreply.github.com>
2018-07-02 16:05:49 +0200

    Add Config Check

--- configuration/Config.go
+++ configuration/Config.go
@@ -3,10 +3,10 @@ package configuration
 import (
 	"github.com/pitwch/go-proffix-geocode/shared"
 	"os"
-	"fmt"
 	"encoding/json"
 	"github.com/pitwch/go-proffix-geocode/geocode"
 	"flag"
+	"log"
 )
 
 type Configuration struct {
@@ -18,25 +18,23 @@ type ErrorConfig struct {
 	field string
 	error string
 }
+
 // Read Config from config.json
 func Config() {
 
 	configuration := Configuration{}
 
-
 	c := new(Configuration)
 	flag.StringVar(&c.Database.Database, "Database", "", "PROFFIX Database Name")
-	flag.IntVar(&c.Database.Port ,"Port",1433, "Database Port")
-	flag.StringVar(&c.Database.Username ,"Username","", "Database Username")
-	flag.StringVar(&c.Database.Password ,"Password","", "Database Password")
-	flag.StringVar(&c.Database.Hostname ,"Hostname","", "Database Hostname")
-	flag.StringVar(&c.Database.Parameter ,"Parameter","?connection+timeout=30", "Database Parameter")
+	flag.IntVar(&c.Database.Port, "Port", 1433, "Database Port")
+	flag.StringVar(&c.Database.Username, "Username", "", "Database Username")
+	flag.StringVar(&c.Database.Password, "Password", "", "Database Password")
+	flag.StringVar(&c.Database.Hostname, "Hostname", "", "Database Hostname")
+	flag.StringVar(&c.Database.Parameter, "Parameter", "?connection+timeout=30", "Database Parameter")
 
-
-	flag.StringVar(&c.Settings.UseGeocoder ,"UseGeocoder","Google", "Geocoding Provider")
-	flag.StringVar(&c.Settings.GoogleAPIKey ,"GoogleAPIKey","", "Google API-Key")
-	flag.StringVar(&c.Settings.MapQuestAPIKey ,"MapQuestAPIKey","", "Mapquest API-Key")
-
+	flag.StringVar(&c.Settings.UseGeocoder, "UseGeocoder", "Google", "Geocoding Provider")
+	flag.StringVar(&c.Settings.GoogleAPIKey, "GoogleAPIKey", "", "Google API-Key")
+	flag.StringVar(&c.Settings.MapQuestAPIKey, "MapQuestAPIKey", "", "Mapquest API-Key")
 
 	// Read config.json. Config has to be next to executable.
 	file, _ := os.Open("config.json")
@@ -44,22 +42,17 @@ func Config() {
 	decoder := json.NewDecoder(file)
 	err := decoder.Decode(&configuration)
 	if err != nil {
-		fmt.Printf("Error reading Config File: %v", err)
+		log.Println("Error reading Config File: %v", err)
 		os.Exit(1)
 	}
 
 	// Check for minimal configuration
-	if (configuration.Database.Database == "" || configuration.Database.Password == "" || configuration.Database.Username == "" || configuration.Database.Hostname == "")  {
-		fmt.Print("Please check the config.json. More info available here: https://github.com/pitwch/go-proffix-geocode#konfiguration \n")
-	os.Exit(1)
+	if (configuration.Database.Database == "" || configuration.Database.Password == "" || configuration.Database.Username == "" || configuration.Database.Hostname == "") {
+		log.Println("Please check the config.json. More info available here: https://github.com/pitwch/go-proffix-geocode#konfiguration \n")
+		os.Exit(1)
 	}
 
 	database.Connect(configuration.Database)
 	geocode.SetSettings(configuration.Settings)
-
-
-
-
-
 
 }
